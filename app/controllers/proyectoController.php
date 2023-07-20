5c07c818646e4be482b45c9a4704c253e8396ca7
ramirez <ramirezcoronelcarlos@gmail.com>
2023-07-20 08:40:21 -0400

    listar tutores

--- app/controllers/proyectoController.php
+++ app/controllers/proyectoController.php
@@ -3,25 +3,34 @@ namespace App\controllers;
 
 use App\proyecto;
 use App\estudiante;
+use App\tutor;
 
 class proyectoController extends controller{
 
     private $proyecto;
     private $estudiantes;
+    private $tutores;
 
     function __construct() {
         $this->proyecto = new proyecto();
         $this->estudiantes = new estudiante();
+        $this->tutores = new tutor();
     }
 
     public function index() {      
 
         $proyectos = $this->proyecto->all();
 
+        $tutores = $this->tutores->all();
+
         $estudiantes = $this->estudiantes->listPendingForProject();
 
 
-        return $this->view('proyectos/gestionar', ['proyectos'=>$proyectos]);
+        return $this->view('proyectos/gestionar', [
+            'proyectos'=>$proyectos,
+            'estudiantes'=>$estudiantes,
+            'tutores'=>$tutores
+        ]);
     }
 
     public function E501() {
