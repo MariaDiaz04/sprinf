ba1800a96b149c17af01ce6393eec15fe2639d37
ramirez <ramirezcoronelcarlos@gmail.com>
2023-09-22 17:32:10 -0400

    full dump actualizado

--- config/bd/dbschema.sql
+++ config/bd/dbschema.sql
@@ -28,8 +28,9 @@ CREATE TABLE `sprinf_bd`.`seccion` (
 
 CREATE TABLE `sprinf_bd`.`malla_curricular` (
   `codigo` varchar(255) UNIQUE PRIMARY KEY,
+  `materia_id` varchar(255),
   `fase_id` varchar(255),
-  `materia_id` varchar(255)
+  `ponderacion` float
 );
 
 CREATE TABLE `sprinf_bd`.`materias` (
@@ -39,19 +40,13 @@ CREATE TABLE `sprinf_bd`.`materias` (
   `htind` int,
   `ucredito` int,
   `hrs_acad` int,
-  `eje` varchar(255)
+  `eje` varchar(255),
+  `cursable` bool DEFAULT true
 );
 
 CREATE TABLE `sprinf_bd`.`profesor` (
   `codigo` varchar(255) UNIQUE PRIMARY KEY,
   `persona_id` int
-);
-
-CREATE TABLE `sprinf_bd`.`clase` (
-  `codigo` varchar(255) UNIQUE PRIMARY KEY,
-  `profesor_id` varchar(255),
-  `seccion_id` varchar(255),
-  `unidad_curricular_id` varchar(255)
 );
 
 CREATE TABLE `sprinf_bd`.`dimension` (
@@ -75,9 +70,12 @@ CREATE TABLE `sprinf_bd`.`estudiante` (
 
 CREATE TABLE `sprinf_bd`.`inscripcion` (
   `id` int UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  `clase_id` varchar(255),
+  `profesor_id` varchar(255),
+  `seccion_id` varchar(255),
+  `unidad_curricular_id` varchar(255),
   `estudiante_id` varchar(255),
-  `calificacion` float
+  `calificacion` float,
+  `estatus` int DEFAULT 1
 );
 
 CREATE TABLE `sprinf_bd`.`proyecto` (
@@ -94,28 +92,11 @@ CREATE TABLE `sprinf_bd`.`proyecto` (
   `cerrado` bool DEFAULT false
 );
 
-CREATE TABLE `sprinf_bd`.`proyecto_historico` (
-  `id` int UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  `nombre_estudiante` varchar(255),
-  `cedula_estudiante` int,
-  `nombre_proyecto` varchar(255),
-  `comunidad` varchar(255),
-  `area` varchar(255),
-  `motor_productivo` varchar(255),
-  `resumen` varchar(255),
-  `direccion` varchar(255),
-  `municipio` varchar(255),
-  `parroquia` varchar(255),
-  `nota_fase_1` float,
-  `nota_fase_2` float,
-  `periodo_inicio` date,
-  `periodo_final` date
-);
-
 CREATE TABLE `sprinf_bd`.`integrante_proyecto` (
   `id` int UNIQUE PRIMARY KEY AUTO_INCREMENT,
   `estudiante_id` varchar(255),
-  `proyecto_id` int
+  `proyecto_id` int,
+  `estatus` int DEFAULT 1
 );
 
 CREATE TABLE `sprinf_bd`.`notas_integrante_proyecto` (
@@ -137,15 +118,10 @@ CREATE TABLE `sprinf_bd`.`persona` (
 
 CREATE TABLE `sprinf_bd`.`usuario` (
   `id` int UNIQUE PRIMARY KEY AUTO_INCREMENT,
+  `rol_id` int,
   `email` varchar(255),
   `contrasena` varchar(255),
   `token` varchar(255)
-);
-
-CREATE TABLE `sprinf_bd`.`roles_usuarios` (
-  `id` int UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  `rol_id` int,
-  `usuario_id` int
 );
 
 CREATE TABLE `sprinf_bd`.`roles` (
@@ -168,17 +144,22 @@ CREATE TABLE `sprinf_bd`.`permisos` (
   `eliminar` bool
 );
 
-CREATE TABLE `sprinf_bd`.`bitacora` (
-  `id` int(11) NOT NULL,
-  `fecha` date NOT NULL,
-  `navegador` varchar(105) NOT NULL,
-  `hora_inicio` time NOT NULL,
-  `hora_cierre` time DEFAULT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `apellido` varchar(80) NOT NULL,
-  `token` varchar(85) NOT NULL,
-  `usuario_id` int(11) NOT NULL
-) ;
+CREATE TABLE `sprinf_bd`.`proyecto_historico` (
+  `nombre_estudiante` varchar(255),
+  `cedula_estudiante` int,
+  `nombre_proyecto` varchar(255),
+  `comunidad` varchar(255),
+  `area` varchar(255),
+  `motor_productivo` varchar(255),
+  `resumen` varchar(255),
+  `direccion` varchar(255),
+  `municipio` varchar(255),
+  `parroquia` varchar(255),
+  `nota_fase_1` float,
+  `nota_fase_2` float,
+  `periodo_inicio` date,
+  `periodo_final` date
+);
 
 ALTER TABLE `sprinf_bd`.`trayecto` ADD FOREIGN KEY (`periodo_id`) REFERENCES `sprinf_bd`.`periodo` (`id`);
 
@@ -188,17 +169,11 @@ ALTER TABLE `sprinf_bd`.`fase` ADD FOREIGN KEY (`siguiente_fase`) REFERENCES `sp
 
 ALTER TABLE `sprinf_bd`.`seccion` ADD FOREIGN KEY (`trayecto_id`) REFERENCES `sprinf_bd`.`trayecto` (`codigo`);
 
-ALTER TABLE `sprinf_bd`.`malla_curricular` ADD FOREIGN KEY (`fase_id`) REFERENCES `sprinf_bd`.`fase` (`codigo`);
-
 ALTER TABLE `sprinf_bd`.`malla_curricular` ADD FOREIGN KEY (`materia_id`) REFERENCES `sprinf_bd`.`materias` (`codigo`);
 
+ALTER TABLE `sprinf_bd`.`malla_curricular` ADD FOREIGN KEY (`fase_id`) REFERENCES `sprinf_bd`.`fase` (`codigo`);
+
 ALTER TABLE `sprinf_bd`.`profesor` ADD FOREIGN KEY (`persona_id`) REFERENCES `sprinf_bd`.`persona` (`cedula`);
-
-ALTER TABLE `sprinf_bd`.`clase` ADD FOREIGN KEY (`profesor_id`) REFERENCES `sprinf_bd`.`profesor` (`codigo`);
-
-ALTER TABLE `sprinf_bd`.`clase` ADD FOREIGN KEY (`seccion_id`) REFERENCES `sprinf_bd`.`seccion` (`codigo`);
-
-ALTER TABLE `sprinf_bd`.`clase` ADD FOREIGN KEY (`unidad_curricular_id`) REFERENCES `sprinf_bd`.`malla_curricular` (`codigo`);
 
 ALTER TABLE `sprinf_bd`.`dimension` ADD FOREIGN KEY (`unidad_id`) REFERENCES `sprinf_bd`.`malla_curricular` (`codigo`);
 
@@ -206,7 +181,11 @@ ALTER TABLE `sprinf_bd`.`indicadores` ADD FOREIGN KEY (`dimension_id`) REFERENCE
 
 ALTER TABLE `sprinf_bd`.`estudiante` ADD FOREIGN KEY (`persona_id`) REFERENCES `sprinf_bd`.`persona` (`cedula`);
 
-ALTER TABLE `sprinf_bd`.`inscripcion` ADD FOREIGN KEY (`clase_id`) REFERENCES `sprinf_bd`.`clase` (`codigo`);
+ALTER TABLE `sprinf_bd`.`inscripcion` ADD FOREIGN KEY (`profesor_id`) REFERENCES `sprinf_bd`.`profesor` (`codigo`);
+
+ALTER TABLE `sprinf_bd`.`inscripcion` ADD FOREIGN KEY (`seccion_id`) REFERENCES `sprinf_bd`.`seccion` (`codigo`);
+
+ALTER TABLE `sprinf_bd`.`inscripcion` ADD FOREIGN KEY (`unidad_curricular_id`) REFERENCES `sprinf_bd`.`malla_curricular` (`codigo`);
 
 ALTER TABLE `sprinf_bd`.`inscripcion` ADD FOREIGN KEY (`estudiante_id`) REFERENCES `sprinf_bd`.`estudiante` (`id`);
 
@@ -222,13 +201,8 @@ ALTER TABLE `sprinf_bd`.`notas_integrante_proyecto` ADD FOREIGN KEY (`integrante
 
 ALTER TABLE `sprinf_bd`.`persona` ADD FOREIGN KEY (`usuario_id`) REFERENCES `sprinf_bd`.`usuario` (`id`);
 
-ALTER TABLE `sprinf_bd`.`roles_usuarios` ADD FOREIGN KEY (`rol_id`) REFERENCES `sprinf_bd`.`roles` (`id`);
-
-ALTER TABLE `sprinf_bd`.`roles_usuarios` ADD FOREIGN KEY (`usuario_id`) REFERENCES `sprinf_bd`.`usuario` (`id`);
+ALTER TABLE `sprinf_bd`.`usuario` ADD FOREIGN KEY (`rol_id`) REFERENCES `sprinf_bd`.`roles` (`id`);
 
 ALTER TABLE `sprinf_bd`.`permisos` ADD FOREIGN KEY (`rol_id`) REFERENCES `sprinf_bd`.`roles` (`id`);
 
 ALTER TABLE `sprinf_bd`.`permisos` ADD FOREIGN KEY (`modulo_id`) REFERENCES `sprinf_bd`.`modulo` (`id`);
-
-ALTER TABLE `sprinf_bd`.`bitacora` ADD FOREIGN KEY (`usuario_id`) REFERENCES `sprinf_bd`.`usuario` (`id`);
-
