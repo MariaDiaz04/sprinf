092dc456e70bfc398af503b8f1c1f2d5ede8b508
ramirez <ramirezcoronelcarlos@gmail.com>
2023-09-26 13:03:08 -0400

    documentacion de trait de cifrado

--- app/traits/utility.php
+++ app/traits/utility.php
@@ -2,18 +2,50 @@
 
 namespace App\Traits;
 
+use Exception;
 
 /**
  * Trait General para funciones Helpers
  */
 trait Utility
 {
+
+  /**
+   * Creación de llaves RSA
+   * Se utiliza el algorimo sha512
+   * y se define una creacion de llaves de 4096
+   *
+   * @return boolean
+   */
+  function generarLlaves(): bool
+  {
+
+    $config = array(
+      "digest_alg" => 'sha512',
+      "private_key_bits" => 4096,
+      "private_key_type" => OPENSSL_KEYTYPE_RSA,
+    );
+
+    // Crear llaves
+    $key = openssl_pkey_new($config);
+    if (!$key) throw new Exception('Key was not generated');
+
+    return true;
+  }
+
   /**
    * ENCRIPTADO
+   *
+   * @param [type] $data
+   * @return string base64 encoded hash
    */
   public function encriptar($data): string
   {
     $path_to_public = $_ENV['PATH_TO_PUBLIC_KEY'];
+
+    if (!file_exists($path_to_public)) {
+      $this->generarLlaves;
+    }
 
     $public_key = openssl_pkey_get_public(file_get_contents($path_to_public));
     $keyData = openssl_pkey_get_details($public_key);
@@ -23,6 +55,13 @@ trait Utility
     return base64_encode($encrypted);
   }
 
+  /**
+   * Desencriptado de información
+   * espera que el hash esté en formato base65
+   *
+   * @param [type] $encryptedData
+   * @return string|null retorna null en caso de haber un error de desencriptado
+   */
   function desencriptar($encryptedData): string | null
   {
     $path_to_private = $_ENV['PATH_TO_PRIVATE_KEY'];
