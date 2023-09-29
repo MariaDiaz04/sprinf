db3873b4dc03803c5a30139b48a2ee3bd0d54eaf
ramirez <ramirezcoronelcarlos@gmail.com>
2023-09-29 15:08:55 -0400

    carga visual de campos formulario proyecto historico

--- src/views/proyectos/gestionar.php
+++ src/views/proyectos/gestionar.php
@@ -246,101 +246,136 @@
 
                     </div>
                   </div>
+                  <hr>
+                  <div class="row form-group">
+                    <div class="col-lg-3">
+                      <label class="form-label" for="tlf_tin">Teléfono</label>
+                      <input type="number" class="form-control mb-1" placeholder="..." name="tlf_tin" id="tlf_tin">
+                    </div>
+                    <div class="col-lg-3">
+                      <label class="form-label" for="parroquia">Parroquia</label>
+                      <input type="text" class="form-control mb-1" placeholder="..." name="parroquia" readonly>
+                    </div>
+                    <div class="col-lg-6">
+                      <label class="form-label" for="comunidad">Comunidad</label>
+                      <textarea class="form-control" placeholder="..." id="comunidad" name="comunidad" style="height: 50px " readonly></textarea>
+                    </div>
+                  </div>
+                  <div class="row form-group mb-2">
+
+                    <div class="col-lg-12">
+                      <label class="form-label" for="resumen">Dirección</label>
+                      <textarea class="form-control" placeholder="..." id="resumen" name="resumen" style="height: 50px" readonly></textarea>
+                    </div>
+                  </div>
+                  <div class="row form-group mb-2">
+                    <div class="col-lg-6">
+                      <label class="form-label" for="tutor_in">Tutor Interno</label>
+                      <input type="text" class="form-control mb-1" placeholder="..." name="tutor_in">
+                    </div>
+
+                    <div class="col-lg-6">
+                      <label class="form-label" for="tutor_ex">Tutor Externo</label>
+                      <input type="text" class="form-control mb-1" placeholder="..." name="tutor_ex">
+                    </div>
+
+                  </div>
                 </div>
               </div>
             </div>
-            <hr>
-            <div class="transfer">
-
-            </div>
-            <hr class="border-light m-0">
-            <div class="text-right mt-3" style="text-align: end;">
-              <input type="submit" class="btn btn-primary" value='Guardar Registro' />&nbsp;
-            </div>
-          </form>
         </div>
+        <hr>
+        <div class="transfer">
+
+        </div>
+        <hr class="border-light m-0">
+        <div class="text-right mt-3" style="text-align: end;">
+          <input type="submit" class="btn btn-primary" value='Guardar Registro' />&nbsp;
+        </div>
+        </form>
       </div>
     </div>
   </div>
+</div>
 
-  <script src="<?= APP_URL ?>assets/js/jquery.transfer.js"></script>
-  <script>
-    let fetchHistoricalUrl = "<?= APP_URL . $this->Route('proyectos/historico') ?>";
-    dataProyectos = []
+<script src="<?= APP_URL ?>assets/js/jquery.transfer.js"></script>
+<script>
+  let fetchHistoricalUrl = "<?= APP_URL . $this->Route('proyectos/historico') ?>";
+  dataProyectos = []
 
 
-    var groupDataArray1 = <?= json_encode($historicoEstudiantes); ?>;
+  var groupDataArray1 = <?= json_encode($historicoEstudiantes); ?>;
 
-    console.log(groupDataArray1)
+  console.log(groupDataArray1)
 
-    var settings3 = {
-      groupDataArray: groupDataArray1,
-      groupItemName: "nombre",
-      groupArrayName: "integrantes",
-      itemName: "nombre",
-      valueName: "value",
-      rightTabNameText: 'Estudiantes Seleccionados',
-      tabNameText: 'Estudiantes del Historico',
-      searchPlaceholderText: 'Buscar Estudiantes',
-      callable: function(items) {
-        console.dir(items);
-      },
-    };
+  var settings3 = {
+    groupDataArray: groupDataArray1,
+    groupItemName: "nombre",
+    groupArrayName: "integrantes",
+    itemName: "nombre",
+    valueName: "value",
+    rightTabNameText: 'Estudiantes Seleccionados',
+    tabNameText: 'Estudiantes del Historico',
+    searchPlaceholderText: 'Buscar Estudiantes',
+    callable: function(items) {
+      console.dir(items);
+    },
+  };
 
-    var transfer = $(".transfer").transfer(settings3);
+  var transfer = $(".transfer").transfer(settings3);
 
-    $('#proyectoGuardarHistorico').submit(function(e) {
-      e.preventDefault()
-      formData = $(this).serializeArray();
-      items = transfer.getSelectedItems();
-      data = [...formData, ...items];
-      console.log(data)
+  $('#proyectoGuardarHistorico').submit(function(e) {
+    e.preventDefault()
+    formData = $(this).serializeArray();
+    items = transfer.getSelectedItems();
+    data = [...formData, ...items];
+    console.log(data)
+  })
+</script>
+<script>
+  let fetchStudentsUrl = "<?= APP_URL . $this->Route('proyectos/pending-students') ?>";
+
+
+
+  $(document).ready(() => {
+    $('#historico').modal('show')
+
+    $('#cargarHistoricoBtn').click(function(e) {
+
     })
-  </script>
-  <script>
-    let fetchStudentsUrl = "<?= APP_URL . $this->Route('proyectos/pending-students') ?>";
 
+    toggleLoading(false)
 
+    // DATATABLE CRUD
 
-    $(document).ready(() => {
-      $('#historico').modal('show')
-
-      $('#cargarHistoricoBtn').click(function(e) {
-
-      })
-
-      toggleLoading(false)
-
-      // DATATABLE CRUD
-
-      // las acciones son definidas en la clase que contiene el botón, es decir,
-      // si necesito editar, le añado la clase "edit"
-      // luego en la función table.on(). verifico si la clase del boton en el que hice click
-      // contiene el nombre de alguna acción que haya definido
+    // las acciones son definidas en la clase que contiene el botón, es decir,
+    // si necesito editar, le añado la clase "edit"
+    // luego en la función table.on(). verifico si la clase del boton en el que hice click
+    // contiene el nombre de alguna acción que haya definido
 
 
 
 
-      let table = new DataTable('#example', {
-        ajax: '<?= $this->Route('proyectos/ssp') ?>',
-        processing: true,
-        serverSide: true,
-        pageLength: 30,
+    let table = new DataTable('#example', {
+      ajax: '<?= $this->Route('proyectos/ssp') ?>',
+      processing: true,
+      serverSide: true,
+      pageLength: 30,
 
-        columnDefs: [{
-          visible: false,
-          targets: [0, 5]
-        }, {
-          data: null,
-          render: function(data, type, row, meta) {
-            return row[6] == 1 ? `<span class="badge rounded-pill bg-success">Evaluado</span>` : `<span class="badge rounded-pill bg-secondary">Pendiente a Evaluar</span>`
-          },
-          targets: 6
-        }, {
+      columnDefs: [{
+        visible: false,
+        targets: [0, 5]
+      }, {
+        data: null,
+        render: function(data, type, row, meta) {
+          return row[6] == 1 ? `<span class="badge rounded-pill bg-success">Evaluado</span>` : `<span class="badge rounded-pill bg-secondary">Pendiente a Evaluar</span>`
+        },
+        targets: 6
+      }, {
 
-          data: null,
-          render: function(data, type, row, meta) {
-            return `<div class="dropdown show">
+        data: null,
+        render: function(data, type, row, meta) {
+          return `<div class="dropdown show">
                       <button class="btn btn-primary btn-icon rounded-pill dropdown-toggle hide-arrow" href="#" role="button" id="dropdown-${row[0]}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       <i class="bx bx-dots-vertical-rounded"></i>
                       </button>
@@ -350,88 +385,88 @@
                         <a class="dropdown-item text-danger" onClick="remove('${row[0]}') href="#">Eliminar</a>
                       </div>
                     </div>`;
-          }, // combino los botons de acción
-          targets: 7 // la columna que representa, empieza a contar desde 0, por lo que la columna de acciones es la 3ra
-        }]
+        }, // combino los botons de acción
+        targets: 7 // la columna que representa, empieza a contar desde 0, por lo que la columna de acciones es la 3ra
+      }]
+    });
+
+
+
+
+
+    $('#guardar').submit(function(e) {
+      e.preventDefault()
+
+      toggleLoading(true);
+
+
+      url = $(this).attr('action');
+      data = $(this).serializeArray();
+
+
+
+
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        error: function(error, status) {
+          toggleLoading(false)
+          alert(error.responseText)
+        },
+        success: function(data, status) {
+          table.ajax.reload();
+          // usar sweetalerts
+          document.getElementById("guardar").reset();
+          // actualizar tabla
+          toggleLoading(false)
+        },
       });
 
+    })
 
+    function edit(id) {
+      alert(`Editing ${id}`)
+    }
 
+    function remove(id) {
+      alert(`Removing ${id}`)
+    }
 
-
-      $('#guardar').submit(function(e) {
-        e.preventDefault()
-
-        toggleLoading(true);
-
-
-        url = $(this).attr('action');
-        data = $(this).serializeArray();
-
-
-
-
-        $.ajax({
-          type: "POST",
-          url: url,
-          data: data,
-          error: function(error, status) {
-            toggleLoading(false)
-            alert(error.responseText)
-          },
-          success: function(data, status) {
-            table.ajax.reload();
-            // usar sweetalerts
-            document.getElementById("guardar").reset();
-            // actualizar tabla
-            toggleLoading(false)
-          },
-        });
-
-      })
-
-      function edit(id) {
-        alert(`Editing ${id}`)
+    // TOGGLE BUTTON AND SPINNER
+    function toggleLoading(show) {
+      if (show) {
+        $('#guardarLoading').show();
+        $('#guardarSubmit').hide();
+      } else {
+        $('#guardarLoading').hide();
+        $('#guardarSubmit').show();
       }
 
-      function remove(id) {
-        alert(`Removing ${id}`)
-      }
+    }
 
-      // TOGGLE BUTTON AND SPINNER
-      function toggleLoading(show) {
-        if (show) {
-          $('#guardarLoading').show();
-          $('#guardarSubmit').hide();
-        } else {
-          $('#guardarLoading').hide();
-          $('#guardarSubmit').show();
-        }
+    $('#anadirEstudiante').click(function(e) {
+      e.preventDefault();
 
-      }
-
-      $('#anadirEstudiante').click(function(e) {
-        e.preventDefault();
-
-        let studentsAlreadyAppened = document.getElementById("cuerpoTablaEstudiantes").children.length;
+      let studentsAlreadyAppened = document.getElementById("cuerpoTablaEstudiantes").children.length;
 
 
 
-        if (studentsAlreadyAppened >= 5) {
-          alert('limite de estudiantes alcanzado');
-        } else {
-          let selectedStudent = $('#selectEstudiante option:selected');
+      if (studentsAlreadyAppened >= 5) {
+        alert('limite de estudiantes alcanzado');
+      } else {
+        let selectedStudent = $('#selectEstudiante option:selected');
 
-          let studentId = $(selectedStudent).val();
+        let studentId = $(selectedStudent).val();
 
-          if (studentId) {
+        if (studentId) {
 
-            if ($("#cuerpoTablaEstudiantes").find(`#appenedStudent-${studentId}`).length > 0) {
-              alert('Estudiante ya ha sido añadido')
-              return false;
-            }
+          if ($("#cuerpoTablaEstudiantes").find(`#appenedStudent-${studentId}`).length > 0) {
+            alert('Estudiante ya ha sido añadido')
+            return false;
+          }
 
-            let fila = `<tr id="appenedStudent-${studentId}" class="studentRow">
+          let fila = `<tr id="appenedStudent-${studentId}" class="studentRow">
                       <th scope="row">
                       <input type="text" name="integrantes[]" class="form-control-plaintext" value="${studentId}" hidden>
                       ${$(selectedStudent).data('cedula')}
@@ -440,68 +475,68 @@
                       <td>${$(selectedStudent).data('apellido')}</td>
                       <td><button type="button" class="btn btn-secondary" onClick="removeStudent('${studentId}')">Eliminar</button></td>
                     </tr>`;
-            $('#cuerpoTablaEstudiantes').append(fila);
-          }
+          $('#cuerpoTablaEstudiantes').append(fila);
         }
-      })
-
-      $('#proyectoGuardar').submit(function(e) {
-        e.preventDefault()
-        url = $(this).attr('action');
-        data = $(this).serializeArray();
-
-        $.ajax({
-          type: "POST",
-          url: url,
-          data: data,
-          error: function(error, status) {
-            toggleLoading(false)
-            alert(error.responseText)
-          },
-          success: function(data, status) {
-            table.ajax.reload();
-            // usar sweetalerts
-            document.getElementById("proyectoGuardar").reset();
-            // actualizar tabla
-            toggleLoading(false)
-          },
-        });
-      })
-
-      $('#selectFaseId').change(function(e) {
-        let selectedFase = $('#selectFaseId option:selected');
-
-        let faseId = $(selectedFase).val();
-
-        fetchEstudiantes(faseId);
-      })
-
-      function fetchEstudiantes(idFase) {
-        $.ajax({
-          type: "POST",
-          url: fetchStudentsUrl,
-          data: {
-            'idFase': idFase
-          },
-          error: function(error, status) {
-            toggleLoading(false)
-            alert(error.responseText)
-          },
-          success: function(data, status) {
-            estudiantes = JSON.parse(data)
-            if (estudiantes) {
-              renderSelectList(estudiantes)
-            } else {
-              $('#selectEstudiante').find('option').remove()
-            }
-          },
-        });
       }
+    })
 
-      function renderSelectList(data) {
-        $('#selectEstudiante').find('option').remove()
-        data.forEach(estudiante => {
-          let row = `<option 
+    $('#proyectoGuardar').submit(function(e) {
+      e.preventDefault()
+      url = $(this).attr('action');
+      data = $(this).serializeArray();
+
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        error: function(error, status) {
+          toggleLoading(false)
+          alert(error.responseText)
+        },
+        success: function(data, status) {
+          table.ajax.reload();
+          // usar sweetalerts
+          document.getElementById("proyectoGuardar").reset();
+          // actualizar tabla
+          toggleLoading(false)
+        },
+      });
+    })
+
+    $('#selectFaseId').change(function(e) {
+      let selectedFase = $('#selectFaseId option:selected');
+
+      let faseId = $(selectedFase).val();
+
+      fetchEstudiantes(faseId);
+    })
+
+    function fetchEstudiantes(idFase) {
+      $.ajax({
+        type: "POST",
+        url: fetchStudentsUrl,
+        data: {
+          'idFase': idFase
+        },
+        error: function(error, status) {
+          toggleLoading(false)
+          alert(error.responseText)
+        },
+        success: function(data, status) {
+          estudiantes = JSON.parse(data)
+          if (estudiantes) {
+            renderSelectList(estudiantes)
+          } else {
+            $('#selectEstudiante').find('option').remove()
+          }
+        },
+      });
+    }
+
+    function renderSelectList(data) {
+      $('#selectEstudiante').find('option').remove()
+      data.forEach(estudiante => {
+        let row = `<option 
             value="${estudiante.id}" 
             data-cedula="${estudiante.cedula}" 
             data-nombre="${estudiante.nombre}" 
@@ -509,13 +544,13 @@
             ${estudiante.cedula} - ${estudiante.nombre} ${estudiante.apellido}
           </option>`;
 
-          $('#selectEstudiante').append(row);
-        });
-      }
-
-    })
-
-    function removeStudent(id) {
-      $(`#appenedStudent-${id}`).remove()
+        $('#selectEstudiante').append(row);
+      });
     }
-  </script>
+
+  })
+
+  function removeStudent(id) {
+    $(`#appenedStudent-${id}`).remove()
+  }
+</script>
